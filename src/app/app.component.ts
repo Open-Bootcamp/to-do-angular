29ff1a9f74caa9765a4bc7c7ec9b68caf765cd7f
lvanegasimagine <lvanegas1429@gmail.com>
2022-09-17 10:07:59 -0600

    Interface done, Header done, Mobile First done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-angular';
+  mode='light';
+
+  constructor(@Inject(DOCUMENT)private document: Document){
+    this.document.body.style.background =  this.mode === 'light'?"hsl(236, 33%, 92%)":"hsl(235, 21%, 11%)";
+  }
+  changeMode(){
+    this.mode === 'light'?this.mode = 'dark':this.mode='light';
+    this.document.body.style.background =  this.mode === 'light'?"hsl(236, 33%, 92%)":"hsl(235, 21%, 11%)";
+  }
 }
