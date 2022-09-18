4358dbcc1fdc7f2a525d0c58bba41f79b7585265
Osmar Ortiz <xizted79@gmail.com>
2022-09-18 16:33:34 -0400

    Added toggle status completed to task

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from 'src/app/interface/Task.interface';
+import { TodoService } from 'src/app/services/todo/todo.service';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +10,11 @@ import { Task } from 'src/app/interface/Task.interface';
 export class CardComponent implements OnInit {
   @Input() task: Task;
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {}
+
+  toggleTodo() {
+    this.todoService.toggleTodo(this.task.id);
+  }
 }
--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Task } from 'src/app/interface/Task.interface';
 
 const tasks: Task[] = [
-  { id: 1, description: 'test', completed: false },
+  { id: 1, description: 'test', completed: true },
   { id: 2, description: 'test2', completed: false },
 ];
 
@@ -18,5 +18,14 @@ export class TodoService {
 
   add(newTask: Task) {
     tasks.push(newTask);
+  }
+
+  toggleTodo(id: number) {
+    tasks.forEach((task, index) => {
+      if (task.id === id) {
+        tasks[index] = { ...task, completed: !task.completed };
+        return;
+      }
+    });
   }
 }
